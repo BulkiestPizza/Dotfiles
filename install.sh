1913157f3357d6fb0d076bf83f131229b0176cd3
Lachlan <lachlanleoknell@gmail.com>
2021-07-31 23:35:44 +1000

    install.sh

--- install.sh
+++ install.sh
@@ -4,36 +4,16 @@ vim= cat $HOME/.vimrc
 polybar= cat $HOME/.config/polybar
 kitty= cat $HOME/.config/kitty
 
-if [[ -z $i3 ]];
-then
+#i3
 	mv ~/.config/i3/config ~/.config/i3/config.old;
 	cp ./config ~/.config/i3;
-else
-	cp ./config ~/.config/i3;
-fi
-
-if [[ -z $vim ]];
-then
+#vim
 	mv ~/.vimrc ~/.vimrc.old;
 	cp ./vimrc ~/.vimrc;
-else
-	cp ./vimrc ~/.vimrc;
-fi
-
-if [[ -z $polybar ]];
-then
+#polybar
 	mv ~/.config/polybar ~/.config/polybar.old;
 	cp -r ./polybar ~/.config/polybar;
-else
-	cp -r ./polybar ~/.config/polybar;
-fi
-
-if [[ -z $kitty ]];
-then
+#kitty
 	mv ~/.config/kitty ~/.config/kitty.old;
 	cp -r ./kitty ~/.config/kitty;
-else
-	cp -r ./kitty ~/.config/kitty;
-fi
-
 
